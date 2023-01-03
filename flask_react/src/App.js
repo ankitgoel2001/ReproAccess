4d60a752fc0d86d27bd67c3b25a0a1ebbb8c0e79
Ankit Goel <ankgoel@ucdavis.edu>
2023-01-02 20:18:15 -0800

    finished design

--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -42,42 +42,84 @@ function FormQuestions({props}) {
   )
 }
 
+function DisplayOrgDetails({props}) {
+  let orgDisplay = []
+  const orgBoxColor = {
+    'backgroundColor': props[1]
+  }
+  console.log(props[1])
+
+  for (let i = 0; i < 5; i++) {
+    orgDisplay.push(
+      <div className={styles.orgBox} style={orgBoxColor}>
+        Organization {i}
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.orgDivider}>
+      <h3 className={styles.orgTitle}>
+        {props[0]}
+      </h3>
+      <div className={styles.orgList}>
+        {orgDisplay}
+      </div>
+    </div>
+  )
+}
+
+function DisplayOrganizations() {
+  const orgMap = {
+      'title1': ['Sponsoring organizations', 'green'],
+      'title2': ['Flight sponsors', 'orange'],
+      'title3': ['Meal services', 'blue'],
+      'title4': ['Carpooling', 'gray'],
+      'title5': ['Healthcare funding', 'red']
+    }
+  
+  let rows = []
+  for (const [key, value] of Object.entries(orgMap)) {
+      rows.push(<div>
+        <DisplayOrgDetails props={value}/>   
+      </div>)
+  }
+
+  return (
+    <div className={styles.orgCollection}>
+      {rows}
+    </div>
+  ) 
+}
+
 function FormDetail() {
+  const [showOrgList, setShowOrgList] = useState(false)
+
   const detailHeader = {
     textAlign: "center"
   };
 
-  return (
-    <div id="profileForm" className={styles.formFill}>
-      <h3 style={detailHeader}>Enter details</h3>
-      <div className={styles.formFillData}>
-        <FormQuestions props={"Name"}/>
-        <FormQuestions props={"Age"}/>
-        <FormQuestions props={"Gender"}/>
-        <FormQuestions props={"State"}/>
-        <FormQuestions props={"Phone Number"}/>
-        <FormQuestions props={"Nationality"}/>
-        <FormQuestions props={"Citizenship Status"}/>
-        <FormQuestions props={"Reason for assistance?"}/>
-        <FormQuestions props={"Annual Income"}/>
-        <FormQuestions props={"Availability?"}/>
-        <button id="formSubmitButton" className={styles.formButton}>Submit</button>
-      </div>
-    </div>
+  const formQuestions = ['Name', 'Age', 'Gender', 'State', 'Phone Number', 'Nationality', 'Citizenship Status', 'Reason for Assistance?', 'Annual Income', 'Availability?']
+  let questionsList = []
 
-  )
-}
-
-function RenderResource(){ 
-  const [resources, setResource] = useState(FormDetail())
+  for (let i = 0; i < formQuestions.length; i++) {
+    questionsList.push(<FormQuestions props={formQuestions[i]} />)
+  }
 
   return (
     <div>
-      
+      <div id="formFillBox" className={styles.formFill}>
+        <h3 style={detailHeader}>Enter details</h3>
+        <div className={styles.formFillData}>
+          {questionsList}
+          <button onClick={() => {setShowOrgList(true)}} id="formSubmitButton" className={styles.formButton}>Submit</button>
+        </div>
+      </div>
+
+      {showOrgList && <DisplayOrganizations />}
     </div>
   )
 }
-
 
 //the main function
 function App() {
